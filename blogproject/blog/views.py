59ef68986c17f7589a8f5aa4e1a43aff4d2aa065
zy_yu <37924017+soiger@users.noreply.github.com>
2018-06-14 12:58:10 +0800

    version 0.1

---
+++ blogproject/blog/views.py
@@ -0,0 +1,31 @@
+from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from . models import Post,Category
+import markdown
+
+def index(request):
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request,'blog/index.html',{'post_list':post_list})
+
+def detail(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    # 记得在顶部引入 markdown 模块
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                     'markdown.extensions.extra',
+                                     'markdown.extensions.codehilite',
+                                     'markdown.extensions.toc',
+                                  ])
+    return render(request, 'blog/detail.html', {'post': post})
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', {'post_list': post_list})
+
+def category(request, pk):
+    # 记得在开始部分导入 Category 类
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
